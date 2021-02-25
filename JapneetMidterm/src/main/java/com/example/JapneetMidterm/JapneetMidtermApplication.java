2bd4f158f9242aa7065af55bf5a47594b2809cfb
Japneet98 <75054199+Japneet98@users.noreply.github.com>
2021-02-25 15:08:56 -0800

    Add files via upload

---
+++ JapneetMidterm/src/main/java/com/example/JapneetMidterm/JapneetMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.example.JapneetMidterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapneetMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapneetMidtermApplication.class, args);
+	}
+
+}
---
+++ JapneetMidterm/src/test/java/com/example/JapneetMidterm/JapneetMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JapneetMidterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapneetMidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
